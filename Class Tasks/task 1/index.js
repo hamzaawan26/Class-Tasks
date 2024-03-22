f86f482da2d45fb4e845e2f5b2242343cca85af6
hamzaawan26 <134953398+hamzaawan26@users.noreply.github.com>
2024-03-22 22:11:01 +0500

    Initial Commit

---
+++ Class Tasks/task 1/index.js
@@ -0,0 +1,15 @@
+function update() {
+  console.log('Button clicked!')
+}
+
+function onMouseDown() {
+  console.log('Mouse Down')
+}
+
+function onMouseEnter() {
+  console.log('Mouse entered')
+}
+
+function onMouseLeave() {
+  console.log('Mouse left')
+}
