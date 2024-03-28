6e1749c39c77de2c8ee1c6ae75b238b71de77bac
hamzaawan26 <134953398+hamzaawan26@users.noreply.github.com>
2024-03-28 19:37:14 +0500

    Delete Class Tasks directory

--- Class Tasks/task 2/practice.js
+++
@@ -1,7 +0,0 @@
-function update() {
-  console.log('updating')
-
-  var input = document.getElementById('myinput')
-  var output = document.getElementById('output')
-  output.innerHTML = input.value
-}
